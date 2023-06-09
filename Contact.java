3fc55ac182ff08d672fcd46c96a869a1880eb924
Karlina Dwi Septiani <114347630+krlnds@users.noreply.github.com>
2023-06-09 19:35:12 +0700

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,18 @@
+public class Contact {
+
+    String name;
+    String number;
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+}
